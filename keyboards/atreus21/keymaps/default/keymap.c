49e85faa98e07d4a83436a2378d4b1624db33566
Stefan Schwarz <stefan@f2o.io>
2020-04-13 22:12:05 +0200

    hi there rainbow

--- keyboards/atreus21/keymaps/default/keymap.c
+++ keyboards/atreus21/keymaps/default/keymap.c
@@ -32,9 +32,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // afterboot
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  debug_enable=true;
-  debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWIRL+5);
 }
